f8a387714b0180d2706445b8b952ad5e06d73fd2
Maxim Milovanov <mirovan@narod.ru>
2017-11-12 20:22:29 +0300

    начал настройку spring security

---
+++ src/main/java/ru/myrecord/SecurityConfig.java
@@ -0,0 +1,13 @@
+package ru.myrecord;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by max on 12.11.2017.
+ */
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    //ToDo: настроить Spring Security
+}

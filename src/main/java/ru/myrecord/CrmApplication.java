2319134a1f837cc14dae135f06331be251d304f1
Mirovan <mirovan@narod.ru>
2018-05-20 15:31:11 +0300

    update for tomcat

--- src/main/java/ru/myrecord/CrmApplication.java
+++ src/main/java/ru/myrecord/CrmApplication.java
@@ -6,21 +6,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class CrmApplication extends SpringBootServletInitializer {
-//public class CrmApplication {
 
-	/**
-	 * Run via tomcat
-	 * */
 	public static void main(String[] args) {
 		SpringApplication.run(CrmApplication.class, args);
-		//SpringApplication.run(new Class<?>[] {CrmApplication.class, JpaConfig.class}, args);
 	}
 
-	/**
-	 * Run via GAE - https://github.com/GoogleCloudPlatform/getting-started-java/tree/master/appengine-standard-java8/springboot-appengine-standard
-	 * */
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(CrmApplication.class);
